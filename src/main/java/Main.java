7e8017cef49cb1714c48925f3ca103e4b0cad0fe
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-25 21:12:45 +0300

    Update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,21 +12,12 @@ public class Main {
         double celsius = (int) temp.fahrenheitToCelsius(fahrenheit);
         System.out.println(fahrenheit + " in Celsius: " + celsius);
 
-        // 32F = 0C
-        // 68F = 20C
-        // 212F = 100C
-        System.out.println(temp.fahrenheitToCelsius(32));
-        System.out.println(temp.fahrenheitToCelsius(68));
-        System.out.println(temp.fahrenheitToCelsius(212));
-
-        // 10C = 50F
-        // 20C = 68F
-        // 50C = 122F
-        // 100C = 212F
-        System.out.println(temp.celsiusToFahrenheit(10));
-        System.out.println(temp.celsiusToFahrenheit(20));
-        System.out.println(temp.celsiusToFahrenheit(50));
-        System.out.println(temp.celsiusToFahrenheit(100));
+        System.out.println("Convert Fahrenheit to Celsius");
+        System.out.println("Give temperature in Fahrenheit: ");
+        int celsius1 = scanner.nextInt();
+        double fahrenheit1 = (int) temp.celsiusToFahrenheit(celsius1);
+        System.out.println(celsius1 + " in Fahrenheit: " + fahrenheit1);
+        System.out.println();
 
         System.out.println(temp.isExtremeTemperature(20));
         System.out.println(temp.isExtremeTemperature(50));
