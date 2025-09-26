ac1782dcfd85af166b88fe5a177dc67c10773c88
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-26 11:15:57 +0300

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,21 +10,28 @@ public class Main {
         System.out.println("Give temperature in Fahrenheit: ");
         int fahrenheit = scanner.nextInt();
         double celsius = (int) temp.fahrenheitToCelsius(fahrenheit);
-        System.out.println(fahrenheit + " in Celsius: " + celsius);
+        System.out.println(fahrenheit + " Fahrenheit in Celsius is " + celsius);
         System.out.println();
 
         System.out.println("Convert Celsius to Fahrenheit");
         System.out.println("Give temperature in Celsius: ");
         int celsius1 = scanner.nextInt();
         double fahrenheit1 = (int) temp.celsiusToFahrenheit(celsius1);
-        System.out.println(celsius1 + " in Fahrenheit: " + fahrenheit1);
+        System.out.println(celsius1 + " Celsius in Fahrenheit is " + fahrenheit1);
         System.out.println();
 
         System.out.println("Convert Kelvin to Celsius");
         System.out.println("Give temperature in Kelvin: ");
         int kelvin = scanner.nextInt();
         double celsius2 = (int) temp.kelvinToCelsius(kelvin);
-        System.out.println(kelvin + " in Fahrenheit: " + celsius2);
+        System.out.println(kelvin + " Kelvin in Celsius is " + celsius2);
+        System.out.println();
+
+        System.out.println("Is temperature extreme? ");
+        System.out.println("Give temperature in Celsius: ");
+        int celsiusExtreme = scanner.nextInt();
+        boolean isExtreme = temp.isExtremeTemperature(celsiusExtreme);
+        System.out.println(isExtreme);
         System.out.println();
     }
 }
